be234bd86859b0f3683612a29be64dc24c295450
brunomarks@gmail.com <bruno.marques@renato-cunha.mkt.virtual>
2010-09-10 19:28:53 -0300

    update

---
+++ default.js
@@ -0,0 +1,18 @@
+var URL_BASE = 'http://www.mktvirtual.com.br/lab/easyfloater/';
+
+var yoursite = {
+  init : function(){
+        easyFloater.init({
+          width : 600, //your div width in pixels
+          height : 620, //your div heigth in pixels
+          top : 20, //distance of top in pixels
+          id : 'floater', //'your flash identification (<embed id="floater">)'
+          color : 'transparent', //your div background color 
+          flashVars : {URL_BASE : URL_BASE}, //object with flashvars content
+          src : URL_BASE + "swf/floater.swf",
+          alternativeBg : URL_BASE + 'imagens/alternative.jpg', //bg if iPad or iPhone
+		  alternativeLink : 'http://www.google.com.br'
+        });  
+  }
+}
+window.load = yoursite.init();
